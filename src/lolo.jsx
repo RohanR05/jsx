891ce73450813d0cf9d7f60d18424ee8af833660
003-demo <rrriku05@gmail.com>
2026-04-12 18:53:31 +0600

    first commit

---
+++ src/lolo.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Rr = () => {
+  const [count, setCount] = useState(0);
+
+  const handleCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  };
+  return (
+    <div>
+      <h2>Count:{count} </h2>
+      <button onClick={handleCount}>Click</button>
+    </div>
+  );
+};
+
+export default Rr;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Rr from './lolo'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Rr></Rr>
+  </StrictMode>,
+)
