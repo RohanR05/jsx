0a73a634d31c37a3658da33530e357d5ab6eb352
003-demo <rrriku05@gmail.com>
2026-04-25 23:52:22 +0600

    x

--- src/FormManagement.jsx
+++ src/FormManagement.jsx
@@ -1,13 +1,19 @@
-import Form from './Form'
-import FormTable from './FormTable'
+import { useState } from "react";
+import Form from "./Form";
+import FormTable from "./FormTable";
 
 const FormManagement = () => {
+  const [products, setProducts] = useState([]);
+  const handleAddProducts = (newProduct) => {
+    const newProducts = [...products, newProduct];
+    setProducts(newProducts);
+  };
   return (
     <div>
-      <Form></Form>
+      <Form handleAddProducts={handleAddProducts}></Form>
       <FormTable></FormTable>
     </div>
-  )
-}
+  );
+};
 
-export default FormManagement
+export default FormManagement;
